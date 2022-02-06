b3eba332cedac3c7c850ce86f1239970dfe95734
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-02-05 20:44:53 -0700

    handled submit using ref

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,21 +1,28 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
-  const [enteredName, setEnteredName] = useState();
+  // const [enteredName, setEnteredName] = useState();
+  const inputRef = useRef();
 
-  const nameInputChangeHandler = (e) => {
-    setEnteredName(e.target.value);
-  };
+  // const nameInputChangeHandler = (e) => {
+  //   setEnteredName(e.target.value);
+  // };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
-    console.log(enteredName);
+    // console.log(enteredName);
+    const enteredValue = inputRef.current.value;
   };
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
-        <input type="text" id="name" onChange={nameInputChangeHandler} />
+        <input
+          type="text"
+          id="name"
+          // onChange={nameInputChangeHandler}
+          ref={inputRef}
+        />
       </div>
       <div className="form-actions">
         <button>Submit</button>
