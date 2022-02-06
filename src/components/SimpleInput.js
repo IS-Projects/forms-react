f2c451d02fbb2a15cb4a5c60deb74c776990a3cd
Tejada-Joyce <joyce_105_3@hotmail.com>
2022-02-05 20:58:59 -0700

    bound value to input

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,27 +1,33 @@
 import { useState, useRef } from "react";
 
 const SimpleInput = (props) => {
-  // const [enteredName, setEnteredName] = useState();
+  const [enteredName, setEnteredName] = useState();
   const inputRef = useRef();
 
-  // const nameInputChangeHandler = (e) => {
-  //   setEnteredName(e.target.value);
-  // };
+  const nameInputChangeHandler = (e) => {
+    setEnteredName(e.target.value);
+  };
 
   const formSubmitHandler = (e) => {
     e.preventDefault();
-    // console.log(enteredName);
+    console.log(enteredName);
+
     const enteredValue = inputRef.current.value;
+    console.log(enteredValue);
+
+    // inputRef.current.value = "" => NOT IDEAL! DON'T MANIPULATE THE DOM!
+    setEnteredName("");
   };
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
         <label htmlFor="name">Your Name</label>
         <input
+          ref={inputRef}
           type="text"
           id="name"
-          // onChange={nameInputChangeHandler}
-          ref={inputRef}
+          onChange={nameInputChangeHandler}
+          value={enteredName}
         />
       </div>
       <div className="form-actions">
